e25c65a7c3bbbe274a3a016b8671ee4627bcbda1
Prasad Kothavale <prasadkothavale@yahoo.co.in>
2020-03-21 21:46:41 +0000

    Add retry in internet status

--- internet_indicator.sh
+++ internet_indicator.sh
@@ -15,13 +15,19 @@ case $1 in
         done
         ;;
     check)
-        internet_flag=`ping -q -w 1 -c 1 8.8.8.8 | grep -c "1 received"`
-        if [ $internet_flag -eq 1 ] 
-        then
-            echo "0" > /sys/class/gpio/gpio4/value
-        else
-            echo "1" > /sys/class/gpio/gpio4/value
-        fi
+        retry=3
+        internet_flag=0
+        while [ $retry -gt 0 ] && [ $internet_flag -eq 0 ]
+        do
+            ((retry=retry-1))
+            internet_flag=`ping -q -w 1 -c 1 8.8.8.8 | grep -c "1 received"`
+            if [ $internet_flag -eq 1 ] 
+            then
+                echo "0" > /sys/class/gpio/gpio4/value
+            else
+                echo "1" > /sys/class/gpio/gpio4/value
+            fi
+        done
         ;;
     *)
         echo 'error: require atleast one argument'
